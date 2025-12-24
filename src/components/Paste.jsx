c9f3f2650897a37ed970a5e8321d8da112e5411c
Suman Bera <135948367+ONEABOVEALL01@users.noreply.github.com>
2025-12-25 00:32:34 +0530

    Remove unused import from Paste component

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,4 +1,4 @@
-import { div } from "motion/react-client";
+
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeFromPastes } from "../redux/pasteSlice";
