c6cbbc56b5136e279bec72c6f1ae04dbeac178ff
Suman Bera <135948367+ONEABOVEALL01@users.noreply.github.com>
2025-12-25 19:59:39 +0530

    Refactor NavBar styles and layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className="navBar flex  h-[8vh] mt-4 text-center bg-[#004D61] justify-between gap-[20vh] text-[#E5E7EB] text-2xl">
+    <div className="navBar flex h-[8vh] mt-4 text-center bg-gray-800 justify-between gap-[10vh] text-[#E5E7EB] text-2xl">
       <div>
         <p className="text-green-300 ml-[10vh]  font-semibold mt-[1.5vh]">ONEABOVEALL</p>
       </div>
 
-      <div className="mr-[30vh] mt-[1.5vh] flex">
-        <div className="mr-[20vh]">
+      <div className="mr-[40vh] mt-[1.5vh] flex">
+        <div className="mr-[10vh]">
           <NavLink to="/">Home</NavLink>
         </div>
 
@@ -22,3 +22,4 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
