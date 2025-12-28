aa4ed8c140396588f02c8ff0bc23283a2c259f4f
Suman Bera <135948367+ONEABOVEALL01@users.noreply.github.com>
2025-12-28 19:40:46 +0530

    Update Home component styling with gradient background

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -66,7 +66,7 @@ const Home = () => {
     setSearchParams({});
   }
   return (
-    <div>
+    <div className=" bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-size-[6rem_4rem]">
      
          <div className="flex justify-center">
         <input
